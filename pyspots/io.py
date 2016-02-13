e1e1e247d6e17a545ec376214ec1c7c26f561069
Eric Dill <edill@bnl.gov>
2016-02-13 11:22:53 -0500

    Add docs

--- pyspots/io.py
+++ pyspots/io.py
@@ -1,12 +1,42 @@
 from collections import namedtuple
 import numpy as np
+
+# Store each column of output from Ramdog as a 'spot' namedtuple
 spot = namedtuple('spot', ['idx', 'q', 'phi', 'x', 'y', 'data'])
 
+
 def read_normalized_file(filepath):
+    """
+    Read in the Ramdog output file that ends with '_normalized_spot.txt'
+
+    Parameters
+    ----------
+    filepath : str
+        The path to the output file
+
+    Returns
+    -------
+    spots : list
+        List of `spot` namedtuples that contain the following fields.
+        idx : int
+            Incremental index that goes from 0 -> num_spots
+        q : float
+            The q value of the spot center
+        phi : float
+            The angluar position on the 2-D detector of the spot center
+        x : float
+            Sub-pixel resolution spot center in x
+        y :
+            Sub-pixel resolution spot center in y
+        data : array
+            The intensity data of the spot in all frames
+    """
     spots = []
     with open(filepath, 'r') as f:
         gen = (line for line in f.readlines())
         indices = next(gen).split()[1:]
+        # Discard the min/max values of the spot because `np.min()` and
+        # `np.max()` are a thing
         min_values = next(gen)
         max_values = next(gen)
         q_vals = next(gen).split()[1:]
